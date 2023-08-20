c7d70a07c35814cd90a7d9e65d6cf8c298115d1d
Antte <aheatta@hotmail.com>
2023-08-20 09:00:32 +0300

    push

---
+++ src/main/java/com/weatherwebsite/weatherwebsite/WeatherwebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.weatherwebsite.weatherwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherwebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherwebsiteApplication.class, args);
+	}
+
+}
